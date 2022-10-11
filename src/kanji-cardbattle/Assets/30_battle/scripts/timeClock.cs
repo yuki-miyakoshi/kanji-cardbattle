63bf0c175b1da5e372f655807f5c4fe4e9a1e6b1
miyakoshi-yuki <miyakoshi2000@icloud.com>
2022-10-11 09:20:26 +0900

    ゲーム主要機能搭載

--- src/kanji-cardbattle/Assets/30_battle/scripts/timeClock.cs
+++ src/kanji-cardbattle/Assets/30_battle/scripts/timeClock.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+
 public class timeClock : MonoBehaviour {
+
   public Image UIobj;
-  public bool isStart;
+  public bool doStart;
   public float countTime = 5.0f;
+  public bool isBushu;
 
   void Update (){
-    if (isStart) {
+    if (doStart) {
       UIobj.fillAmount -= 1.0f / countTime * Time.deltaTime;
     }
+
+    if( UIobj.fillAmount == 0){
+      gameObject.SetActive (false);
+      if( !isBushu ){
+        GameManager.instance.doNextTsukuri++;
+      }
+      if( isBushu ){
+        GameManager.instance.doNextBushu = true;
+        GameObject.Find("Kanji").SetActive (false);
+      }
+      
+    }
+
   }
 }
